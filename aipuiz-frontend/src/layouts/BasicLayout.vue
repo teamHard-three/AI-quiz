9dd27a78ead58526e92333bd5188f3e74b35ce2b
“Wolf-Van” <2805569574@qq.com>
2025-07-12 22:58:21 +0800

    update front

---
+++ aipuiz-frontend/src/layouts/BasicLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>AI-quiz</el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            :default-active="activeIndex"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+          >
+            <el-menu-item index="1" @click="navigateTo('/home')">
+              <el-icon><setting /></el-icon>
+              <span>首页</span>
+            </el-menu-item>
+            <el-menu-item index="2" @click="navigateTo('/about')">
+              <el-icon><setting /></el-icon>
+              <span>关于</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main> <RouterView :key="$route.fullPath" /></el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { onMounted, watch } from 'vue'
+const router = useRouter()
+
+const activeIndex = ref('1')
+const navigateTo = (path) => {
+  router.push(path)
+}
+const setActiveIndexByRoute = () => {
+  if (router.path === '/home') {
+    activeIndex.value = '1'
+  } else if (router.path === '/about') {
+    activeIndex.value = '2'
+  }
+}
+watch(
+  () => router.path,
+  () => {
+    setActiveIndexByRoute()
+  },
+)
+onMounted(() => {
+  router.push('/home')
+  setActiveIndexByRoute()
+})
+</script>
+
+<style scoped></style>
