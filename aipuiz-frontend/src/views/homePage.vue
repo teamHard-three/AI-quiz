dbf060a9e0f85857f005e2ad1f5df509e104fa94
lixinheng <2780957705@qq.com>
2025-07-23 22:19:42 +0800

    选课同意和不同意按钮纠错

--- aipuiz-frontend/src/views/homePage.vue
+++ aipuiz-frontend/src/views/homePage.vue
@@ -1529,20 +1529,20 @@ div[style*='justify-content: flex-end'] button:hover {
 }
 
 /* 选课申请同意按钮 */
-.course-table .approve {
+.course-table .approve-btn {
   background: linear-gradient(90deg, #67c23a 0%, #b3e19d 100%);
   color: #fff;
 }
-.course-table .approve:hover {
+.course-table .approve-btn:hover {
   background: linear-gradient(90deg, #529b2e 0%, #67c23a 100%);
 }
 
 /* 选课申请不同意按钮 */
-.course-table .reject {
+.course-table .reject-btn {
   background: linear-gradient(90deg, #f56c6c 0%, #fbb1b1 100%);
   color: #fff;
 }
-.course-table .reject:hover {
+.course-table .reject-btn:hover {
   background: linear-gradient(90deg, #d9534f 0%, #f56c6c 100%);
 }
 
